771fd768fb093d9a9ef805ce0e53792134827b86
admin <ryleysill93@gmail.com>
2019-07-31 23:42:19 -0700

    prep for heroku

--- emojis/settings.py
+++ emojis/settings.py
@@ -81,7 +81,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'build/static')
 
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WSGI_APPLICATION = 'emojis.wsgi.application'
 
