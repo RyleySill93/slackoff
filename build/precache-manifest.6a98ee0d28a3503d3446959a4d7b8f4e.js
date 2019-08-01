983baa646e864724faa07dd5356b54ec5b18219b
admin <ryleysill93@gmail.com>
2019-08-01 08:39:18 -0700

    prep for heroku deploy

---
+++ build/precache-manifest.6a98ee0d28a3503d3446959a4d7b8f4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689a772a946aa5fa81e98edff5faa6ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "928261b22eaa6ce714db",
+    "url": "/static/css/main.0e9075fc.chunk.css"
+  },
+  {
+    "revision": "ed38caa15e4e95619ddc",
+    "url": "/static/js/2.b2580084.chunk.js"
+  },
+  {
+    "revision": "928261b22eaa6ce714db",
+    "url": "/static/js/main.c6ecddd6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
