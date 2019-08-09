d1b9d51d091353c09984e05d831456e05df43564
admin <ryleysill93@gmail.com>
2019-08-08 22:01:38 -0700

    aws and image optimization

--- gifs/views.py
+++ gifs/views.py
@@ -4,23 +4,58 @@ import uuid
 from emojis.settings import BASE_DIR
 from django.http import HttpResponse
 import base64
+import boto3
+import os
+from rest_framework.response import Response
+from rest_framework import status
 
+
+S3_BUCKET = os.environ.get('S3_BUCKET')
+
+
+def upload_file(file_path, file_name):
+    s3_client = boto3.client('s3')
+    s3_client.upload_file(file_path, S3_BUCKET, file_name)
+    url = s3_client.generate_presigned_url('get_object',
+                                    Params={
+                                        'Bucket': S3_BUCKET,
+                                        'Key': file_name,
+                                    },
+                                    ExpiresIn=3600)
+
+    return url
+
+
+def prep_file(file):
+    im = Image.open(file)
+    im = im.convert('RGBA')
+
+    # make into a square
+    width, height = im.size
+    size = max(width, height)
+    new_im = Image.new('RGBA', (size, size), 255)
+    new_im.paste(im, (int((size - width) / 2), int((size - height) / 2)))
+
+    # keep background transparent
+    alpha = new_im.split()[3]
+    im = new_im.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=255)
+    mask = Image.eval(alpha, lambda a: 255 if a <= 128 else 0)
+    im.paste(255, mask)
+
+    im = im.resize((256, 256), Image.ANTIALIAS)
+
+    return im
 
 @api_view(['POST'])
 def bobblify(request):
-    start = -30
-    end = 0
+    start = -15
+    end = 15
     degs = 5
     current = start
 
     frames = []
 
-    im = Image.open(request.FILES['file'])
-
-    alpha = im.split()[3]
-    im = im.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=255)
-    mask = Image.eval(alpha, lambda a: 255 if a <= 128 else 0)
-    im.paste(255, mask)
+    im = prep_file(request.FILES['file'])
 
     while current <= end:
         frame = im.rotate(current, fillcolor=255)
@@ -32,61 +67,75 @@ def bobblify(request):
         frames.append(frame)
         current -= degs
 
-    file_name = '{}/pics/{}.gif'.format(BASE_DIR, uuid.uuid1())
+    file_name = '{}.gif'.format(uuid.uuid1())
+    file_path = '{}/pics/{}'.format(BASE_DIR, file_name)
 
     frames[0].save(
-        file_name,
+        file_path,
         save_all=True,
         disposal=2,
         append_images=frames[1:],
         duration=40,
         transparency=255,
-        loop=0
+        loop=0,
+        optimize=True,
+        quality=95,
     )
 
-    image_data = open(file_name, "rb").read()
+    url = upload_file(file_path, file_name)
 
-    meow = base64.b64encode(image_data)
+    os.remove(file_path)
 
-    return HttpResponse(meow, content_type="image/png")
+    return Response({'url': url}, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 def intensify(request):
     frames = []
 
-    im = Image.open(request.FILES['file'])
+    im = prep_file(request.FILES['file'])
 
-    alpha = im.split()[3]
-    im = im.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=255)
-    mask = Image.eval(alpha, lambda a: 255 if a <= 128 else 0)
-    im.paste(255, mask)
+    width, height = im.size
+
+    offset = int(width / 50)
 
     coords = [
-        (10, 10),
-        (-10, 10),
-        (10, -10),
-        (-10, -10),
-        (10, 10),
-        (10, -10),
-        (-10, 10),
+        (offset, offset),
+        (-offset, offset),
+        (offset, -offset),
+        (-offset, -offset),
+        (offset, offset),
+        (offset, -offset),
+        (-offset, offset),
     ]
 
     for x, y in coords:
-        frame = ImageChops.offset(im, x, y)
+        a = 1
+        b = 0
+        c = x  # left/right (i.e. 5/-5)
+        d = 0
+        e = 1
+        f = y  # up/down (i.e. 5/-5)
+        frame = im.transform(im.size, Image.AFFINE, (a, b, c, d, e, f), fillcolor=255)
+
         frames.append(frame)
 
-    file_name = '{}/pics/{}.gif'.format(BASE_DIR, uuid.uuid1())
-    frames[0].save(file_name,
+    file_name = '{}.gif'.format(uuid.uuid1())
+    file_path = '{}/pics/{}'.format(BASE_DIR, file_name)
+
+    frames[0].save(file_path,
                    save_all=True,
                    disposal=2,
                    append_images=frames[1:],
                    duration=40,
                    transparency=255,
-                   loop=0)
+                   loop=0,
+                   optimize=True,
+                   quality=95,
+                   )
 
-    image_data = open(file_name, "rb").read()
+    url = upload_file(file_path, file_name)
 
-    meow = base64.b64encode(image_data)
+    os.remove(file_path)
 
-    return HttpResponse(meow, content_type="image/png")
+    return Response({'url': url}, status=status.HTTP_200_OK)
--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@ import Dropzone from 'react-dropzone'
 
 class App extends React.Component {
     state = {
-        data: '',
+        url: '',
         file: null,
-        style: 'intensify'
+        style: 'intensify',
+        fileUrl: '',
     };
 
     render() {
         const uploadFile = (file) => {
-            this.setState({ file })
+            this.setState({ file, fileUrl: URL.createObjectURL(file) });
         };
 
         const doStuff = () => {
             const formData = new FormData();
             formData.append("file", this.state.file, this.state.file.name);
-            axios.post(`${window.location.origin}/${this.state.style}/`, formData).then(res => {
-                this.setState({ data: res.data })
+            axios.post(`${'http://localhost:8000'}/${this.state.style}/`, formData).then(res => {
+                this.setState({ url: res.data.url })
             });
         };
 
@@ -38,25 +39,55 @@ class App extends React.Component {
                         <div className="step">1. UPLOAD</div>
                         <div className="button-holder">
                             <input type="file" id="upload-photo" hidden onChange={event => uploadFile(event.target.files[0])} />
-                            <button className="button" onClick={openDialog}>SEND PICS? ;)</button>
+                            {
+                                this.state.file ? (
+                                    <React.Fragment>
+                                        <div/>
+                                        <img style={{ width: 100 }} src={this.state.fileUrl} />
+                                        <button className="button" onClick={openDialog}>SEND PICS? ;)</button>
+                                    </React.Fragment>
+                                ) : (
+                                    <React.Fragment>
+                                        <div/>
+                                        <button className="button" onClick={openDialog}>SEND PICS? ;)</button>
+                                        <div/>
+                                    </React.Fragment>
+                                )
+                            }
+
                         </div>
                     </div>
                     <div className="column column-middle">
                         <div className="step">2. SELECT STYLE</div>
                         <div className="button-holder">
+                            <div/>
                             <select className="button" value={this.state.style} onChange={event => this.setState({ style: event.target.value })}>
                                 <option value="bobblify">BOBBLE</option>
                                 <option value="intensify">INTENSIFY</option>
                             </select>
+                            <div/>
                         </div>
                     </div>
                     <div className="column">
                         <div className="step">3. GENERATE EMOJI</div>
                         <div className="button-holder">
                             {
-                                this.state.data ? (
-                                    <img style={{ width: 100 }} src={`data:image/gif;base64,${this.state.data}`} />
-                                ) : <button className="button" onClick={doStuff}>CLICK ME</button>
+                                this.state.url ? (
+                                    <React.Fragment>
+                                        <div/>
+                                        <img style={{ width: 100 }} src={this.state.url} />
+                                        <div className="another-one">
+                                            <img className="dj-khaled" style={{ width: 100 }} src={require('./khaled.png')} />
+                                            <button className="button" onClick={doStuff}>ANOTHER ONE</button>
+                                        </div>
+                                    </React.Fragment>
+                                ) : (
+                                    <React.Fragment>
+                                        <div/>
+                                        <button className="button" onClick={doStuff}>CLICK ME</button>
+                                        <div/>
+                                    </React.Fragment>
+                                )
                             }
                         </div>
                     </div>
